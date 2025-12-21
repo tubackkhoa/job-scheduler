22815db538eacc61be462cffbcc62178c847d274
Pham Tu <tubackkhoa@gmail.com>
2025-12-21 15:57:48 +0700

    remove constraint

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   // @ts-ignore
-  import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000';
+  import.meta.env.VITE_API_BASE_URL ?? '';
 
 export default {
   async fetchSchema(userId: number, pluginId: number) {
