c615d27143e86d5e4514df524ae1b13349fd64cd
kelvin1804 <cuong.nh@orai.io>
2025-12-26 18:26:22 +0700

    Update: remove frontend

--- frontend/src/components/dashboard/fields/index.js
+++
@@ -1,9 +0,0 @@
-import { MLThresholdsTableField } from './MLThresholdsTableField';
-import { MultiSelectField } from './MultiSelectField';
-import { SqlField } from './SqlField';
-
-export default {
-  MLThresholdsTable: MLThresholdsTableField,
-  MultiSelect: MultiSelectField,
-  Sql: SqlField,
-};
