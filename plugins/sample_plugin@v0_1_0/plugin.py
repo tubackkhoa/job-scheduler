ac7eb05f152c4e3b6c7ff87ed9212a552c68334f
Pham Tu <tubackkhoa@gmail.com>
2025-12-28 14:52:02 +0700

    extend type

--- plugins/sample_plugin@v0_1_0/plugin.py
+++ plugins/sample_plugin@v0_1_0/plugin.py
@@ -20,7 +20,9 @@ class Config(BaseModel):
         default_factory=list,
         json_schema_extra={"ui:field": "MultiSelect", "default": "BTC,ETH,SOL,BNB,LINK"},
     )
-    sql: str = Field("1.0", json_schema_extra={"ui:field": "Sql"})
+    sql: str = Field("SELECT 1", json_schema_extra={"ui:field": "Template", "type": "sql"})
+    json_template: str = Field("{}", json_schema_extra={"ui:field": "Template", "type": "json"})
+    yaml_template: str = Field("root:1", json_schema_extra={"ui:field": "Template", "type": "yaml"})
 
     @field_validator("sql", mode="after")
     @classmethod
