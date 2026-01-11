82376128d1d850c70c4e7f17a6da23f731f2a36c
Pham Tu <tubackkhoa@gmail.com>
2026-01-11 16:27:07 +0700

    add schema hint

--- plugins/schema.py
+++ plugins/schema.py
@@ -1,4 +1,22 @@
-from typing import Any, TypedDict, List, cast
+from typing import Any, Literal, TypedDict, List, cast
+
+
+UIWidget = Literal[
+    "datetime",
+    "text",
+    "textarea",
+    "password",
+    "email",
+    "uri",
+    "date",
+    "updown",
+    "range",
+    "checkbox",
+    "radio",
+    "select",
+    "checkboxes",
+    "files",
+]
 
 
 class ModelExpr(TypedDict, total=False):
@@ -11,11 +29,13 @@ class ModelExpr(TypedDict, total=False):
 JSONUISchema = TypedDict(
     "JSONUISchema",
     {
-        "type": str,
+        "type": Literal["string", "number", "sql", "markdown", "js", "yaml", "yml", "json"],
         "enum": list[Any],
         "default": Any,  # default value for uiSchema
-        "ui:field": str,  # for render field
-        "ui:widget": str,  # for render widget
+        "ui:field": Literal[
+            "MLThresholdsTable", "MultiSelect", "Template", "Version"
+        ],  # for render field
+        "ui:widget": UIWidget,  # for render widget
         "ui:expr": str | tuple[str, *tuple[list[str], ...]],  # expression with memo support
         "ui:options": dict,  # standard ui:options for json_schema_form, mean other ui:option should be placed in here
         "model:binding": List[str],  # for binding element with path
